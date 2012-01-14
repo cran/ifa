98270be663df5bb30deb5d33985ad0cef8a3ae97
Cinzia Viroli <cinzia.viroli@unibo.it>
2012-01-14 00:00:00 +0000

    version 7.0

--- R/ifa.init.random.R
+++ R/ifa.init.random.R
@@ -2,8 +2,8 @@
 function(y,L)
 {
 numvar<-ncol(y)
-H<-matrix(runif(L*numvar,-1,1),numvar,L)
-psi<-var(y)
+H<-matrix(runif(L*numvar,-3,3),numvar,L)
+psi<-diag(diag(var(y)))*0.1
 
 output<-list(psi=psi,H=H)
 }
